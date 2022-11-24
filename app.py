1851898cc29ac6810789d723863f1cee8d86b7a7
Donko Dimov <donko@abv.bg>
2022-11-24 18:09:00 +0100

    test: Create model and controller for lists

--- app.py
+++ app.py
@@ -14,17 +14,31 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(), nullable=False)
     completed = db.Column(db.Boolean, nullable=False, default=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('todolists.id'), nullable=False)
 
     def __repr__(self):
         return f'<Todo {self.id} {self.description}>'
 
+class TodoList(db.Model):
+    __tablename__ = 'todolists'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable=False)
+    todos = db.relationship('Todo', backref='list', lazy=True, cascade='all, delete-orphan')
+
+    def __repr__(self):
+        return f'<TodoList {self.id} {self.name}>'
 
 #db.create_all()
 
 
+@app.route('/lists/<list_id>')
+def get_list_todos(list_id):
+    return render_template('index.html', data=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+
 @app.route('/')
 def index():
-    return render_template('index.html', data=Todo.query.order_by('id').all())
+    return redirect(url_for('get_list_todos', list_id=1))
+
 
 @app.route('/todo/<todo_id>/set-completed', methods=['POST'])
 def set_completed(todo_id):
@@ -66,7 +80,6 @@ def create_todo():
 @app.route('/todo/<todo_id>', methods=['DELETE'])
 def delete_todo(todo_id):
     error = False
-    body = {}
     try:
         todo = Todo.query.get(todo_id)
         db.session.delete(todo)
@@ -80,4 +93,4 @@ def delete_todo(todo_id):
     if error:
         abort(400)
     else:
-        return render_template('index.html', data=Todo.query.order_by('id').all())
+        return jsonify({ 'success': True })
